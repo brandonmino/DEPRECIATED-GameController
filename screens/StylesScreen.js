8466a4e4a4fdb02a652fc4a108bd2c92a4e1f461
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-12-25 18:20:26 -0500

    Fixed style stuff and fixed unused things

--- screens/StylesScreen.js
+++ screens/StylesScreen.js
@@ -7,6 +7,8 @@ export default StyleSheet.create({
     },
     optionsContainer: {
         alignContent: 'center',
+        justifyContent: "center",
+        paddingTop: 150,
     },
     homeContainer: {
         flex: 1,
@@ -15,51 +17,18 @@ export default StyleSheet.create({
     homeTitle: {
         fontSize: 28,
         textAlign: 'center',
-        paddingBottom: 30,
+        paddingTop: 100,
     },
     homeOptions: {
         fontFamily: 'space-mono', 
         alignItems: 'center',
-        marginVertical: 1,
+        marginVertical: 5,
         backgroundColor: 'rgba(0,0,0,0.05)',
         borderRadius: 3,
         paddingHorizontal: 4,
     },
     homeLink: {
         paddingVertical: 7,
-    },
-    codeHighlightText: {
-        color: 'rgba(96,100,109, 0.8)',
-    },
-    codeHighlightContainer: {
-        backgroundColor: 'rgba(0,0,0,0.05)',
-        borderRadius: 3,
-        paddingHorizontal: 4,
-    },
-    tabBarInfoContainer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        ...Platform.select({
-            ios: {
-                shadowColor: 'black',
-                shadowOffset: { width: 0, height: -3 },
-                shadowOpacity: 0.1,
-                shadowRadius: 3,
-            },
-            android: {
-                elevation: 20,
-            },
-        }),
-        alignItems: 'center',
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 20,
-    },
-    tabBarInfoText: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        textAlign: 'center',
     },
     navigationFilename: {
         marginTop: 5,
