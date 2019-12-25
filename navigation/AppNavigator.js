9612a58d3ae877f4f29bddeaaec3dfe4e6e58268
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-12-25 15:57:55 -0500

    fix nav items

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
@@ -7,12 +6,8 @@ import CreateScreen from '../screens/CreateScreen';
 import EditScreen from '../screens/EditScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
-
-
 export default createAppContainer(
   createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
     Home: HomeScreen,
     Create: CreateScreen,
--- navigation/AppNavigator.web.js
+++ navigation/AppNavigator.web.js
@@ -4,9 +4,11 @@ import { createSwitchNavigator } from 'react-navigation';
 import MainTabNavigator from './MainTabNavigator';
 
 const switchNavigator = createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
+  Home: HomeScreen,
+  Create: CreateScreen,
+  Edit: EditScreen,
+  Settings: SettingsScreen,
 });
 switchNavigator.path = '';
 
