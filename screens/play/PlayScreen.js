08b77033230fc9f5a3e56401be3c70ac2c3eddc5
Jamie Moschella <jamie@uconn.edu>
2020-02-06 17:25:31 -0500

    Makes buttons draggable

--- screens/play/PlayScreen.js
+++ screens/play/PlayScreen.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import {ScrollView, Text, TouchableOpacity, View, Button} from 'react-native';
 import { styleglobal } from '../StylesScreen'
-import { styleplay } from './StylesPlay'
-//let Draggable = require('react-native-draggable');
+import { styleplay, dragPosition} from './StylesPlay'
+import Draggable from 'react-native-draggable';
 
-// const overflow={
-//   overflowY: 'hidden',
-//   overflowX: 'hidden',
-// };
+
+const overflow={
+  overflowY: 'hidden',
+  overflowX: 'hidden',
+};
 
 export default class CreateScreen extends React.Component {
   render() { 
     return (
-      <View style={styleglobal.homeContainer}>
+      <View style={[styleglobal.homeContainer, overflow]}>
         <View style={styleglobal.homeContainer} contentContainerStyle={styleglobal.contentContainer}>
-          <TouchableOpacity style={[styleplay.newButton, styleplay.BL]} />
-          <TouchableOpacity style={[styleplay.newButton, styleplay.BR]} />
-          <TouchableOpacity style={[styleplay.newButton, styleplay.BU]} />
-          <TouchableOpacity style={[styleplay.newButton, styleplay.BD]} />
-          <TouchableOpacity style={[styleplay.arrowButton, styleplay.AL]} />
-          <TouchableOpacity style={[styleplay.arrowButton, styleplay.AR]} />
-          <TouchableOpacity style={[styleplay.arrowButton, styleplay.AU]} />
-          <TouchableOpacity style={[styleplay.arrowButton, styleplay.AD]} />
+
+          <Draggable {...dragPosition.CL}><TouchableOpacity style={[styleplay.colorButton, styleplay.CL]} /></Draggable>
+          <Draggable {...dragPosition.CR}><TouchableOpacity style={[styleplay.colorButton, styleplay.CR]} /></Draggable>
+          <Draggable {...dragPosition.CU}><TouchableOpacity style={[styleplay.colorButton, styleplay.CU]} /></Draggable>
+          <Draggable {...dragPosition.CD}><TouchableOpacity style={[styleplay.colorButton, styleplay.CD]} /></Draggable>
+          <Draggable {...dragPosition.AL}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
+          <Draggable {...dragPosition.AR}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
+          <Draggable {...dragPosition.AU}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
+          <Draggable {...dragPosition.AD}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
+
           <View style={styleglobal.backContainer}>
             <TouchableOpacity style={styleglobal.backButton} onPress={() => this.props.navigation.navigate('Use')}>
-            <Text>Back</Text>
+              <Text>Back</Text>
             </TouchableOpacity>
           </View>
         </View>
--- screens/play/StylesPlay.js
+++ screens/play/StylesPlay.js
@@ -2,55 +2,65 @@ import {StyleSheet} from 'react-native'
 import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
 
 const styleplay = StyleSheet.create({
-    newButton: {
-        position: "absolute",
+    colorButton: {
+        backgroundColor: 'pink', //Just a placeholder, should never be visible
         width: vw(10),
         height: vw(10),
         borderRadius: vw(5),
     },
     arrowButton: {
-        position: "absolute",
         backgroundColor: 'gray',
         width: vw(9),
         height: vw(9),
         borderRadius: vw(1),
     },
-    BL: {
+    CL: {
         backgroundColor: 'yellow',
-        left: vw(60-5),
-        top: vh(50-10),
     },
-    BR: {
+    CR: {
         backgroundColor: 'blue',
-        left: vw(80-5),
-        top: vh(50-10),
     },
-    BU: {
+    CU: {
         backgroundColor: 'red',
-        left: vw(70-5),
-        top: vh(30-10),
     },
-    BD: {
+    CD: {
         backgroundColor: 'green',
-        left: vw(70-5),
-        top: vh(70-10),
-    },
-    AL: {
-        left: vw(20-4.5),
-        top: vh(50-9),
-    },
-    AR: {
-        left: vw(40-4.5),
-        top: vh(50-9),
-    },
-    AU: {
-        left: vw(30-4.5),
-        top: vh(30-9),
-    },
-    AD: {
-        left: vw(30-4.5),
-        top: vh(70-9),
-    },
+    }
 });
 
-export { styleplay };
+const dragPosition = {
+    CL: {
+        x: vw(60-5),
+        y: vh(50-10),
+    },
+    CR: {
+        x: vw(80-5),
+        y: vh(50-10),
+    },
+    CU: {
+        x: vw(70-5),
+        y: vh(30-10),
+    },
+    CD: {
+        x: vw(70-5),
+        y: vh(70-10),
+    },
+    AL: {
+        x: vw(20-4.5),
+        y: vh(50-9),
+    },
+    AR: {
+        x: vw(40-4.5),
+        y: vh(50-9),
+    },
+    AU: {
+        x: vw(30-4.5),
+        y: vh(30-9),
+    },
+    AD: {
+        x: vw(30-4.5),
+        y: vh(70-9),
+    }
+}
+
+export { styleplay, dragPosition};
