7f91cbf11075ce060495ca73a99821ae4b8f9434
brandonmino <43628774+brandonmino@users.noreply.github.com>
2020-02-06 20:06:26 -0500

    locked things and added json for moving controller data

--- screens/play/PlayScreen.js
+++ screens/play/PlayScreen.js
@@ -10,20 +10,25 @@ const overflow={
   overflowX: 'hidden',
 };
 
+function basicPress(event) {
+  console.log("Hell yeah!");
+  
+}
+
 export default class CreateScreen extends React.Component {
   render() { 
     return (
       <View style={[styleglobal.homeContainer, overflow]}>
         <View style={styleglobal.homeContainer} contentContainerStyle={styleglobal.contentContainer}>
 
-          <Draggable {...dragPosition.CL}><TouchableOpacity style={[styleplay.colorButton, styleplay.CL]} /></Draggable>
-          <Draggable {...dragPosition.CR}><TouchableOpacity style={[styleplay.colorButton, styleplay.CR]} /></Draggable>
-          <Draggable {...dragPosition.CU}><TouchableOpacity style={[styleplay.colorButton, styleplay.CU]} /></Draggable>
-          <Draggable {...dragPosition.CD}><TouchableOpacity style={[styleplay.colorButton, styleplay.CD]} /></Draggable>
-          <Draggable {...dragPosition.AL}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
-          <Draggable {...dragPosition.AR}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
-          <Draggable {...dragPosition.AU}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
-          <Draggable {...dragPosition.AD}><TouchableOpacity style={styleplay.arrowButton} /></Draggable>
+          <Draggable {...dragPosition.CL} disabled={true}><TouchableOpacity style={[styleplay.colorButton, styleplay.CL]} onPress={basicPress}/></Draggable>
+          <Draggable {...dragPosition.CR} disabled={true}><TouchableOpacity style={[styleplay.colorButton, styleplay.CR]} onPress={basicPress}/></Draggable>
+          <Draggable {...dragPosition.CU} disabled={true}><TouchableOpacity style={[styleplay.colorButton, styleplay.CU]} onPress={basicPress}/></Draggable>
+          <Draggable {...dragPosition.CD} disabled={true}><TouchableOpacity style={[styleplay.colorButton, styleplay.CD]} onPress={basicPress}/></Draggable>
+          <Draggable {...dragPosition.AL} disabled={true}><TouchableOpacity style={styleplay.arrowButton} onPress={basicPress}/></Draggable>
+          <Draggable {...dragPosition.AR} disabled={true}><TouchableOpacity style={styleplay.arrowButton} onPress={basicPress}/></Draggable>
+          <Draggable {...dragPosition.AU} disabled={true}><TouchableOpacity style={styleplay.arrowButton} onPress={basicPress}/></Draggable>
+          <Draggable {...dragPosition.AD} disabled={true}><TouchableOpacity style={styleplay.arrowButton} onPress={basicPress}/></Draggable>
 
           <View style={styleglobal.backContainer}>
             <TouchableOpacity style={styleglobal.backButton} onPress={() => this.props.navigation.navigate('Use')}>
