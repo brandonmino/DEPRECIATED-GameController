5ae752549f90f03ef21c584443bf8056b5501384
brandonmino <43628774+brandonmino@users.noreply.github.com>
2020-02-26 22:44:16 -0500

    Added additional routes and details in server

--- server/learnExpress.js
+++ server/learnExpress.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()                     // Instance of express
+const app = express()
+app.use(bodyParser.text())
 
-app.use(bodyParser.text())                // Idk converts to plain text i guess..
+//Port and address for connecting and sending/receiving
+const port = 6969
+const address = '50.28.161.59'
 
-// Respond POST request is made to the homepage
-app.post('/', function (req, res) {
-  let body = req.body
+//Receive POST requests for an initial connection from controller to server
+app.post('/init_connect', function (req, res) {
+  let body_data = JSON.parse(req.body)
 
-  res.send('Connected')
-  console.log(JSON.parse(body))
+  //Body with have the controller config it in
+  //Handle the controller config here
+
+  res.send('Your controller has connected to the server')
+  console.log("Controller has connected to server")
 })
 
-app.listen(6969, '50.28.161.59')          // PC IP in after comma
+//Receive POST requests for buttons from RN front end
+app.post('/data_endpoint', function (req, res) {
+  let body_data = JSON.parse(req.body)
+
+  /*
+  if the given descriptor in body_data is in The controller config json/list/whatever{
+    if the action for that button is true, press the button
+    else (if the button is false), then release the button
+  }
+  else (button doesn't exist), log and send back that the button was not recognized
+  */
+
+  res.send("Button Pressed")
+})
+
+//Listen in on port at that address
+app.listen(port, address)     
